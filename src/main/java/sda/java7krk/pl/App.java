50eea48a7427f6d3ca3fe29e6d63f2b944fd0446
Przemek <fraczek.przemyslaw2@gmail.com>
2018-01-20 12:24:37 +0100

    comit 2

---
+++ src/main/java/sda/java7krk/pl/App.java
@@ -0,0 +1,7 @@
+package sda.java7krk.pl;
+
+
+public class App 
+{
+    public static void main( String[] args){}
+}
---
+++ src/test/java/sda/java7krk/pl/AppTest.java
@@ -0,0 +1,15 @@
+package sda.java7krk.pl;
+
+import junit.framework.Test.*;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import org.junit.Test;
+
+
+public class AppTest{
+    @Test
+    public void testApp()
+    {
+
+    }
+}
