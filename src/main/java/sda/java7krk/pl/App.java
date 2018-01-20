4cd9ca8a2c1ba7e6bed81577181452d143954f85
Przemek <fraczek.przemyslaw2@gmail.com>
2018-01-20 14:47:59 +0100

    comit 3

--- src/main/java/sda/java7krk/pl/App.java
+++
@@ -1,7 +0,0 @@
-package sda.java7krk.pl;
-
-
-public class App 
-{
-    public static void main( String[] args){}
-}
---
+++ src/main/java/sda/java7krk/pl/ebook/registration/Response.java
@@ -0,0 +1,22 @@
+package sda.java7krk.pl.ebook.registration;
+
+ class Response {
+    private boolean success;
+    private String massage;
+
+     public String getMassage() {
+         return massage;
+     }
+
+     public void setMassage(String massage) {
+         this.massage = massage;
+     }
+
+     public boolean isSuccess() {
+         return success;
+     }
+
+     public void setSuccess(boolean success) {
+         this.success = success;
+     }
+ }
